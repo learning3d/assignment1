8096698eb4c2b770b155886cc0a1e1929bd8bdb0
Naga Karumuri <anjaneyulun02@gmail.com>
2024-11-16 18:41:56 -0500

    Refactor mesh rendering code to add batch dimensions for vertices and faces

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import torch
 import pytorch3d
 from starter.utils import get_mesh_renderer, load_cow_mesh
 
-
-#Rendering your first mesh
-
+# Rendering your first mesh
 mesh_render = get_mesh_renderer()
 
 vertices, faces = load_cow_mesh(path="data/cow.obj")
 
-texture_rgb = torch.ones_like(vertices) # N X 3
+vertices = vertices.unsqueeze(0)  # Add a batch dimension
+faces = faces.unsqueeze(0)  # Add a batch dimension
+
+texture_rgb = torch.ones_like(vertices)  # N x 3
 texture_rgb = texture_rgb * torch.tensor([0.7, 0.7, 1])
-texture_rgb = texture_rgb.unsqueeze(0)  
-textures = pytorch3d.renderer.TexturesVertex(texture_rgb) # important
+textures = pytorch3d.renderer.TexturesVertex(texture_rgb)  # important
 
 meshes = pytorch3d.structures.Meshes(
     verts=vertices,
     faces=faces,
-    textures=textures,
+    textures=textures,  # Correctly pass the textures object
 )
 
 cameras = pytorch3d.renderer.FoVPerspectiveCameras(
